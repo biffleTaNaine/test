b84b53f44f84028a6666df5118b7f64928a3dbd3
Biff <48888687+biffleTaNaine@users.noreply.github.com>
2019-08-06 21:34:57 +0200

    Update fed.sh

--- fed.sh
+++ fed.sh
@@ -5,5 +5,9 @@
 # Ensure distro is up to date
 dnf update -y
 
+# add needed repositories
+
+
 # Install gnome minimal/core
-dnf install gnome-desktop3.x86_64 terminator zsh && dnf remove gnome-terminal rhythmbox totem cheese polari inkscape brasero gnome-dictionary gnome-user-guide gnome-packagekit empathy yelp gnome-weather gnome-contacts gnome-software gnome-online-miners tracker gnome-calculator vino tigervnc -y
+dnf install gnome-desktop3.x86_64 terminator zsh ansible VirtualBox-6.0 && dnf remove gnome-terminal rhythmbox totem cheese polari inkscape brasero gnome-dictionary gnome-user-guide gnome-packagekit empathy yelp gnome-weather gnome-contacts gnome-software gnome-online-miners tracker gnome-calculator vino tigervnc -y && dnf clean all
+wget https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_x86_64.rpm && rpm -ihv vagrant_*
