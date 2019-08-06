0e4f3cc1d4fd14ed82ecd70ba98872c1202c81c7
Biff <48888687+biffleTaNaine@users.noreply.github.com>
2019-08-06 21:51:11 +0200

    Update fed.sh

--- fed.sh
+++ fed.sh
@@ -11,3 +11,7 @@ dnf update -y
 # Install gnome minimal/core
 dnf install gnome-desktop3.x86_64 terminator zsh vim ansible virtualBox && dnf remove gnome-terminal rhythmbox totem cheese polari inkscape brasero gnome-dictionary gnome-user-guide gnome-packagekit empathy yelp gnome-weather gnome-contacts gnome-software gnome-online-miners tracker gnome-calculator vino tigervnc -y && dnf clean all
 wget https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_x86_64.rpm && rpm -ihv vagrant_*
+
+# start GDM at boot
+systemctl enable gdm.service
+systemctl set-default graphical.target
