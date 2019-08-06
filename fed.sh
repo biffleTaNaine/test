c8e3ee6866dfc6f6b6eca03589760aa1dc285f28
Biff <48888687+biffleTaNaine@users.noreply.github.com>
2019-08-06 21:05:38 +0200

    Update fed.sh

--- fed.sh
+++ fed.sh
@@ -6,6 +6,4 @@
 dnf update -y
 
 # Install gnome minimal/core
-dnf install gnome-desktop3.x86_64 --excludepkg gnome-terminal rhythmbox totem cheese polari inkscape brasero gnome-dictionary gnome-user-guide gnome-packagekit empathy yelp gnome-weather gnome-contacts gnome-software gnome-online-miners gnome-sushi tracker gnome-calculator gnome-sushi vino
-
-# dnf install gnome-core terminator zsh ufw whois dnsutils htop secure-delete gnome-shell-pomodoro transmission-gtk moka-icon-theme faba-icon-theme -y && dnf remove gnome-terminal rhythmbox totem cheese polari inkscape brasero gnome-dictionary gnome-user-guide gnome-packagekit empathy yelp gnome-weather gnome-contacts gnome-software gnome-online-miners gnome-sushi tracker gnome-calculator gnome-sushi vino && dnf clean -y
+dnf install gnome-desktop3.x86_64 terminator zsh && dnf remove gnome-terminal rhythmbox totem cheese polari inkscape brasero gnome-dictionary gnome-user-guide gnome-packagekit empathy yelp gnome-weather gnome-contacts gnome-software gnome-online-miners tracker gnome-calculator vino tigervnc -y
