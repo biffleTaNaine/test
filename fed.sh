00dce5b933a40e5da14de68fc7f87e2715d39605
Biff <48888687+biffleTaNaine@users.noreply.github.com>
2019-08-07 20:59:45 +0200

    Update fed.sh

--- fed.sh
+++ fed.sh
@@ -9,7 +9,7 @@ dnf update -y
 
 
 # Install gnome minimal/core
-dnf install gnome-desktop3.x86_64 gdm terminator zsh vim ansible firefox evolution xclip gnome-tweaks.noarch -y && dnf remove gnome-terminal rhythmbox totem cheese polari inkscape brasero gnome-dictionary gnome-user-guide gnome-packagekit empathy yelp gnome-weather gnome-contacts gnome-software gnome-online-miners tracker gnome-calculator vino tigervnc -y && dnf clean all
+dnf install gnome-desktop3.x86_64 gdm terminator zsh vim ansible firefox evolution xclip gnome-tweaks.noarch libXScrnSaver libvpx SDL2 -y && dnf remove gnome-terminal rhythmbox totem cheese polari inkscape brasero gnome-dictionary gnome-user-guide gnome-packagekit empathy yelp gnome-weather gnome-contacts gnome-software gnome-online-miners tracker gnome-calculator vino tigervnc -y && dnf clean all
 wget https://download.virtualbox.org/virtualbox/6.0.10/VirtualBox-6.0-6.0.10_132072_fedora29-1.x86_64.rpm && rpm -ivh VirtualBox-6.0-6.0.10_132072_fedora29-1.x86_64.rpm
 wget https://releases.hashicorp.com/vagrant/2.2.5/vagrant_2.2.5_x86_64.rpm && rpm -ihv vagrant_*
 wget https://github.com/VSCodium/vscodium/releases/download/1.36.1/codium-1.36.1-1562700284.el7.x86_64.rpm && rpm -ivh codium-1.36.1-1562700284.el7.x86_64.rpm
